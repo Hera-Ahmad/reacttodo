181df2711385b39247a35bf72f6c4db4a66ba3d7
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-14 23:27:52 +0530

    update app javascript file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css"
+
 
 const App = () => {
 
-  const arr
+  const [todo, settodo] = useState("");
+  const [todos, settodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo !== '') {
+      settodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos])
+      settodo("");
+    }
+  }
+
+  const handleDelete=(id)=>{
+    const deltodo= todos.filter((to) => to.id !==id);
+    settodos([...deltodo]);
+  };
+
+
   return (
-    <div className='App'>
-      Hello World
+    <div className="App">
+      <div className="container">
+        <h1 >Todo List App</h1>
+        <form className="todo-form" onSubmit={handleSubmit}>
+          <input type="text" value={todo} placeholder="What In Your Mind" onChange={(e) => settodo(e.target.value)} />
+          <button type="submit">Go</button>
+        </form>
+
+        <ul className="all-Todos">
+          {todos.map((t) => (
+            <li className="single-Todo">
+              <span className="todo-Text">{t.todo} </span>
+              <button>Edit</button>
+              <button onClick={() => handleDelete(t.id)}> Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
